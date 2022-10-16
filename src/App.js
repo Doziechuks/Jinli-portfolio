9e7975172d9aafa8ec23cc5061d773386ad058e7
doziechuks <doziechuks1010@gmail.com>
2022-10-16 17:55:26 +0100

    created the navbar component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import './App.css';
 import HomePage from './pages/homePage';
 import LoadingPage from './pages/isLoagingPage';
-import { useState } from "react";
-import { useEffect } from 'react';
+import NavBar from './components/navBar';
+import { useState, useEffect } from "react";
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
   useEffect(()=>{
     const loadingTimer = setTimeout(()=>{
       setIsLoading(true)
-      return () => clearTimeout(isLoading)
-    }, 3000)
+      return () => clearTimeout(loadingTimer)
+    }, 1000)
   },[])
   return (
     <div>
-      
-        {
-          isLoading ?
-          <HomePage /> :
-          <LoadingPage />
-        }
-      
+      {isLoading ? (
+        <div>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element={<HomePage />} />
+          </Routes>
+        </ div>
+      ) : (
+        <LoadingPage />
+      )}
     </div>
   );
 }
