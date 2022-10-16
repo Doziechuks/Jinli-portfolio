438c1c7a04da11ef0937e7373ff468aa04a86165
doziechuks <doziechuks1010@gmail.com>
2022-10-16 16:35:01 +0100

    added a loader to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
 import HomePage from './pages/homePage';
+import LoadingPage from './pages/isLoagingPage';
+import { useState } from "react";
+import { useEffect } from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(()=>{
+    const loadingTimer = setTimeout(()=>{
+      setIsLoading(true)
+      return () => clearTimeout(isLoading)
+    }, 3000)
+  },[])
   return (
     <div>
-      <HomePage />
+      
+        {
+          isLoading ?
+          <HomePage /> :
+          <LoadingPage />
+        }
+      
     </div>
   );
 }
---
+++ src/pages/isLoagingPage.js
@@ -0,0 +1,9 @@
+import './isLoadingPage.css';
+
+const LoadingPage = () => {
+  return <div className="loading">
+    <div className="loader"></div>
+  </div>;
+}
+ 
+export default LoadingPage;
