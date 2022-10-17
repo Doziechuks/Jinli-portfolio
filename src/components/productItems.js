b337c30238618e7daeb00a1eaa57c2af9691b97f
doziechuks <doziechuks1010@gmail.com>
2022-10-17 02:55:46 +0100

    added more styles to the product page

--- src/components/productItems.js
+++ src/components/productItems.js
@@ -5,11 +5,15 @@ const ProductItems = ({ products, history, match }) => {
   const {title, imageUrl, linkUrl} = products;
   return (
     <div className="product-item-wrapper">
-      <div
-        className="product-items-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-        onClick={() => history.push(`${match.url}${linkUrl}`)}
-      />
+      <div className="fade-in-box">
+        <div className="fad-in"></div>
+        <div
+          className="product-items-image"
+          style={{ backgroundImage: `url(${imageUrl})` }}
+          onClick={() => history.push(`${match.url}${linkUrl}`)}
+        />
+      </div>
+
       <h1 className="product-section-title">visit {title} page</h1>
     </div>
   );
