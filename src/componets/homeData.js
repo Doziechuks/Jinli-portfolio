682276e001560851e495956aef4859cb959c323b
doziechuks <doziechuks1010@gmail.com>
2022-10-24 23:40:37 +0100

    updated the homepage data

--- src/componets/homeData.js
+++ src/componets/homeData.js
@@ -1,21 +1,21 @@
 const data = [
   {
     id: 1,
-    title: "Lorem ipsum dolor",
+    title: "jinli motorcycle spare parts",
     imageUrl: "./images/powerbike.jpg",
     info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos debitis doloremque aut sint iure tempore. Lorem ipsum dolor sit amet.",
     linkUrl: "products/motorcycle",
   },
   {
     id: 2,
-    title: "Lorem ipsum dolor",
+    title: "complete japanese motorcycles",
     imageUrl: "./images/repair.jpg",
     info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos debitis doloremque aut sint iure tempore. Lorem ipsum dolor sit amet.",
     linkUrl: "products/bicycles",
   },
   {
     id: 3,
-    title: "Lorem ipsum dolor",
+    title: "japan articles",
     imageUrl: "./images/jinliElectronic.jpg",
     info: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos debitis doloremque aut sint iure tempore. Lorem ipsum dolor sit amet.",
     linkUrl: "products/electronics",
