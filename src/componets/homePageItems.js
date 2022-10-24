f6d0e4a67f7f26d18156faf3b7b0c65ae2cdf6b2
doziechuks <doziechuks1010@gmail.com>
2022-10-24 21:02:40 +0100

    rendered the homepage item in a route

--- src/componets/homePageItems.js
+++ src/componets/homePageItems.js
@@ -1,23 +1,29 @@
 import "./homePageItems.css";
 import CustomButton from "./customButton";
+import { withRouter } from "react-router-dom";
 
-const HomePageItems = ({ items }) => {
-  const { title, info, imageUrl } = items;
+const HomePageItems = ({ items, match, history }) => {
+  const { title, info, imageUrl, linkUrl } = items;
   return (
-      <div className="homepageitems-wrapper">
-        <div
-          className="homepageitem-background"
-          style={{
-            backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.5)), url(${imageUrl})`,
-          }}
-        />
-        <div className="homeItem-content">
-          <h1 className="homepage-title">{title}</h1>
-          <p className="homepage-info">{info}</p>
-          <CustomButton isHomePage>read more</CustomButton>
-        </div>
+    <div className="homepageitems-wrapper">
+      <div
+        className="homepageitem-background"
+        style={{
+          backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.5)), url(${imageUrl})`,
+        }}
+      />
+      <div className="homeItem-content">
+        <h1 className="homepage-title">{title}</h1>
+        <p className="homepage-info">{info}</p>
+        <CustomButton
+          isHomePage
+          onClick={() => history.push(`${match.url}${linkUrl}`)}
+        >
+          know more
+        </CustomButton>
       </div>
+    </div>
   );
 };
 
-export default HomePageItems;
+export default withRouter(HomePageItems);
